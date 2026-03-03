240bb0aaaa0ba1733a0782ee63c99ee92dac27ae
nattcnx <193052998+nattcnx@users.noreply.github.com>
2026-03-03 16:02:21 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = '';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyQUJvbCUFzZXQ1CW_EZtJbPMpjshzwrneu7nqcjCDN5wdUMg5Mu2laTO4YvXnPWbIP8A/exec';
